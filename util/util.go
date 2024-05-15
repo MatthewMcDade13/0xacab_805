fd11b129980a47f98c51c715b9c68ba593f2cf34
MatthewMcDade13 <Matthew.McDade13@tuta.io>
2024-05-15 08:25:18 -0700

    add util functions

--- util/util.go
+++ util/util.go
@@ -1,8 +1,22 @@
 package util
 
-import "os"
+import (
+	"os"
+
+	"crypto/rand"
+	"encoding/json"
+	"net/http"
+
+	"github.com/charmbracelet/log"
+)
 
 const DEFAULT_FILE_PERM = 0666
+const (
+	PW_COST      = 32768
+	PW_BLOCKSIZE = 8
+	PW_PARALLEL  = 3
+	PW_HASH_LEN  = 64
+)
 
 func IsValidFsPath(path string) bool {
 	if _, err := os.Stat(path); err == nil {
@@ -11,3 +25,26 @@ func IsValidFsPath(path string) bool {
 		return false
 	}
 }
+
+func DecodePostBody[T any](req *http.Request) (*T, error) {
+	dec := json.NewDecoder(req.Body)
+	var obj T
+	if err := dec.Decode(&obj); err == nil {
+		return &obj, nil
+	} else {
+		return nil, err
+	}
+}
+
+func GenSalt(size uint32) []byte {
+	bs := make([]byte, size)
+	_, err := rand.Read(bs)
+	if err != nil {
+		log.Infof("EOF received when generating salt of size: %d :: %s", size, err)
+	}
+	return bs
+}
+
+// func GenSaltStr(size int) string {
+//
+// }
